68ef9f1a220abc99d46bed52aca9e5b29c4da05d
siam-al-hasan021 <emon8infinite888@gmail.com>
2025-07-09 00:06:34 +0600

    Progress 2
    Updated the project file.

---
+++ HealthE_LoginInterface/src/healthe_logininterface/FXMLDocumentController.java
@@ -0,0 +1,36 @@
+package healthe_logininterface;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Initialization if needed
+    }
+
+    @FXML
+    private void handleLogin(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+    }
+}
