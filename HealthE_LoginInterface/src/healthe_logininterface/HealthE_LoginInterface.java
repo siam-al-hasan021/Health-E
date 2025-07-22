18d2e9273d94cb9bc6b9fee50b933957fd8f9449
siam-al-hasan021 <emon8infinite888@gmail.com>
2025-07-23 00:36:08 +0600

    Doctors reply

--- HealthE_LoginInterface/src/healthe_logininterface/HealthE_LoginInterface.java
+++ HealthE_LoginInterface/src/healthe_logininterface/HealthE_LoginInterface.java
@@ -8,15 +8,16 @@ import javafx.stage.Stage;
 
 public class HealthE_LoginInterface extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+@Override
+public void start(Stage stage) throws Exception {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/healthe_logininterface/FXMLDocument.fxml"));
+    Parent root = loader.load();
 
-        Scene scene = new Scene(root);
-        stage.setTitle("Health-E Login");
-        stage.setScene(scene);
-        stage.show();
-    }
+    Scene scene = new Scene(root);
+    stage.setTitle("Health-E Login");
+    stage.setScene(scene);
+    stage.show();
+}
 
     public static void main(String[] args) {
         launch(args);
