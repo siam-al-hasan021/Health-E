545425d6b485bd64f0c6c20dc26a49d69eab9371
siam-al-hasan021 <emon8infinite888@gmail.com>
2025-07-22 21:18:09 +0600

    Created view option

---
+++ HealthE_LoginInterface/src/healthe_logininterface/ViewPostsController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package healthe_logininterface;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author user
+ */
+public class ViewPostsController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
